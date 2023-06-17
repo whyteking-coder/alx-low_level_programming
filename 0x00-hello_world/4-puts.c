24fcb5684ac2b6a53746739c6a9acb542c45b6f0
solomon <solostanley068@gmail.com>
2023-06-16 23:41:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Reture: Always (succes)
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
-return(0;) 
+return(0); 
 }	
