fa8c8cdadf8ccae9f12b7d88964496e2e0226723
solomon <solostanley068@gmail.com>
2023-06-17 00:31:40 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
  * main - Entry point
  *
- * Reture: Always (succes)
+ * Return: Always (succes)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return(0); 
-}	
+	puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
