1a3b820f07da510bbea3a9c574786f1a53896f3d
solomon <solostanley068@gmail.com>
2023-06-29 07:18:12 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 /**
- * Main - Entry point
- * Description: 'print alphabt except e and q'
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: always 0 (success)
  */
+
 int main(void)
 {
 	char low, e, q;
@@ -11,14 +15,11 @@ int main(void)
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a'; low < 'z'; low++)
-
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		if
-			(low != e && low != q);
-		putchar(low);
+		if (low != e && low != q)
+			putchar(low);
 	}
-
 	putchar('\n');
 
 	return (0);
