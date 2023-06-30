1e21e190148329e1fea96790a034421374c887ab
solomon <solostanley068@gmail.com>
2023-06-30 13:33:13 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,22 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
-	int dest_len, i;
-	
-	for
-	(dest_len = 0; dest[dest_len] != '\0');
-	 (dest_len++);
-	 for (i = 0; i < n && src[i] != '\0');
-	(i++);
+	int i;
+	int j;
 
-        dest[dest_len + i] = src[i];
-	/*should end with a end of string char*/
-	dest[dest_len + i] = '\0';
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 
 }
