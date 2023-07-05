89d3f0be16d4954debe3e33e5d00a1e333515b29
solomon <solostanley068@gmail.com>
2023-07-05 08:34:19 -0700

    0-puts_recursion

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/**/
-void _puts_recursion(char *s)
-{
-        if (*s == 0)
-        {
-                _putchar ('\n');
-                return;
-        }
-        _putchar(*s);
-        _puts_recursion(s + 1);
-}
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("1");
+	return (0);
+}
