a6b60993512d7db1430933d72acca4066c19ceef
solomon <solostanley068@gmail.com>
2023-07-07 21:06:22 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _structure - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (success)
+ */
+char *_structure(char *s, char c)
+{
+	int i = 0;
+		for (; s[i] >= '\0', i++ ;)
+		{
+			if (s[i] == c)
+				return (&s[i]);
+		}
+		return (0);
+}
