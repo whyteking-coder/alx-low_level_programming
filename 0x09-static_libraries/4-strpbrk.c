ec9eec2e480bddbdc5f869e67e6660539b1f2de5
solomon <solostanley068@gmail.com>
2023-07-07 22:10:11 -0700

    create_static_lib.sh

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -8,13 +8,13 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char k;
+	int k;
 
 	while (*s)
 	{
-		for (k = 0; accept[k]; k++)
+		for (k = 0; *accept (k); k++)
 		{
-		if (*s == accept[k])
+		if (*s == *accept (k))
 		return (s);
 		}
 		s++;
