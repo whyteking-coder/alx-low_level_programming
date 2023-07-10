677b322cc3aeab94b86473bfe6a2042f32682ccb
solomon <solostanley068@gmail.com>
2023-07-10 14:40:28 -0700

    libmy.a, main.h

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,23 +1,16 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
  * _strpbrk - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int k;
-
-	while (*s)
-	{
-		for (k = 0; *accept (k); k++)
-		{
-		if (*s == *accept (k))
-		return (s);
-		}
-		s++;
-	}
-	return('\n');
+	return strpbrk(s, accept);
 }
